568561670aaeb313dc704e3e9b4a596fd69307f5
“hash-murali” <muralikrishnasunkara007@gmail.com>
2025-10-22 09:27:08 -0400

    add multithreading to fib series
    # Conflicts:
    #	src/main/java/org/example/FibSeries.java
    #	src/main/java/org/example/Main.java

--- src/main/java/org/example/FibSeries.java
+++ src/main/java/org/example/FibSeries.java
@@ -1,15 +1,46 @@
-//package org.example;
-//
-//public class FibSeries {
-//
-//    public static void printSeries(){
-//        int a=0,b=1,temp=0;
-//        for(int i=0;i<10;i++){
-//            System.out.println(a);
-//            temp = b;
-//            b = a+b;
-//            a=temp;
-//        }
-//    }
-//
-//}
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class FibSeries {
+
+    private Integer a =0;
+    private final Object lock = new Object();
+    private Integer b = 1;
+    private Integer temp =0;
+    private ExecutorService executorService = Executors.newFixedThreadPool(5);
+
+    public void printSeries() {
+        List<Future<Void>> futureList = new ArrayList<>();
+        for (int i = 0; i < 30; i++) {
+
+            Future<Void> future = executorService.submit(()->{
+                synchronized(lock){
+                    System.out.println(a);
+                    temp = b;
+                    b=a+b;
+                    a=temp;
+                }
+                return null;
+
+            });
+            futureList.add(future);
+        }
+
+        for(Future<Void> future:futureList){
+            try{
+                future.get();
+            }catch (Exception ex){
+                ex.printStackTrace();
+            }
+        }
+
+        executorService.shutdown();
+    }
+
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,10 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-////        FibSeries.printSeries();
-//        for (int i = 0; i <10; i++) {
-//            System.out.println("this is hot fix");
-//        }
+
+        FibSeries f = new FibSeries();
+        f.printSeries();
+
+
     }
 }
