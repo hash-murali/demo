24493d3c7a3f44afa91a7ed7477ab2c171252798
“hash-murali” <muralikrishnasunkara007@gmail.com>
2025-10-22 09:34:32 -0400

    fix 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,10 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-//    public static void main(String[] args) {
+    public static void main(String[] args) {
 ////        FibSeries.printSeries();
 //        for (int i = 0; i <10; i++) {
 //            System.out.println("this is hot fix");
 //        }
-//    }
+    }
 }
