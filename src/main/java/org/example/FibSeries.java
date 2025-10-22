f0903a372d576aeaf8eb865c90040d9ba4c9d6b2
“hash-murali” <muralikrishnasunkara007@gmail.com>
2025-10-22 12:37:40 -0400

    cleaned the code and add logging

--- src/main/java/org/example/FibSeries.java
+++ src/main/java/org/example/FibSeries.java
@@ -1,20 +1,34 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+
+import java.util.logging.Logger;
 
 public class FibSeries {
 
-    private Integer a =0;
-    private final Object lock = new Object();
-    private Integer b = 1;
-    private Integer temp =0;
-    private ExecutorService executorService = Executors.newFixedThreadPool(5);
+    private final Logger log = Logger.getLogger(FibSeries.class.getName());
+    private final Object lock;
+    private Integer a, b, temp;
+    private final ExecutorService executorService;
+
+    public FibSeries(){
+        this.lock = new Object();
+        this.a=0;
+        this.b=1;
+        this.temp=0;
+        this.executorService = Executors.newFixedThreadPool(5);
+    }
+
 
     public void printSeries() {
+        log.log(Level.parse("INFO"),"Printing Fib series starts: ");
         List<Future<Void>> futureList = new ArrayList<>();
         for (int i = 0; i < 30; i++) {
 
@@ -35,11 +49,17 @@ public class FibSeries {
             try{
                 future.get();
             }catch (Exception ex){
-                ex.printStackTrace();
+                log.log(Level.parse("SEVERE"), Arrays.toString(ex.getStackTrace()));
             }
         }
-
-        executorService.shutdown();
+        try {
+            executorService.awaitTermination(250, TimeUnit.MILLISECONDS);
+            log.log(Level.parse("INFO"), "Log complete");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }finally {
+            executorService.shutdown();
+        }
     }
 
 
