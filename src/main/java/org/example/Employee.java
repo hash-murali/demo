b3163571483cff65111b3f9c8e536cc5cb1776ff
“hash-murali” <muralikrishnasunkara007@gmail.com>
2025-10-27 10:37:44 -0400

    done with clean up

---
+++ src/main/java/org/example/Employee.java
@@ -0,0 +1,35 @@
+package org.example;
+
+public class Employee {
+    protected int empId;
+    protected String name;
+    protected double salary;
+
+    void calculateSalary(){
+
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,19 @@
 package org.example;
 
+import java.util.Calendar;
+
 public class Main {
     public static void main(String[] args) {
-        FibSeries f = new FibSeries();
-        f.printSeries();
+//        FibSeries f = new FibSeries();
+//        f.printSeries();
+
+        Employee permanentEmp = new PermanentEmp(101,"anil",10000,1500,3);
+        Employee contractEmp = new ContractEmp(102,"ankit",500,10);
+
+        permanentEmp.calculateSalary();
+        contractEmp.calculateSalary();
+
+        System.out.println("permanentEmp : "+permanentEmp.getSalary());
+        System.out.println("contractEmp : "+contractEmp.getSalary());
     }
 }
